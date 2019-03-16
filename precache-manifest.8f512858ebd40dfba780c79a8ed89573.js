edc70068c53db17aeff4e90d3507dcf2a6a9b942
Vadim Tcaregorodtcev <dev.tcaregorodtcev@gmail.com>
2019-03-16 13:39:38 +0300

    Updates

---
+++ precache-manifest.8f512858ebd40dfba780c79a8ed89573.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "dad532478e7c6084b9019aa66e1bbaf6",
+    "url": "/fuckts-maps/static/media/ofont.ru_Azov.dad53247.ttf"
+  },
+  {
+    "revision": "ede525c4a7e9362ff2fd",
+    "url": "/fuckts-maps/static/js/runtime~main.d8a09a74.js"
+  },
+  {
+    "revision": "e052f9bf24cba7f1b726",
+    "url": "/fuckts-maps/static/js/main.73f9e182.chunk.js"
+  },
+  {
+    "revision": "17b5a1b38737f938f2c8",
+    "url": "/fuckts-maps/static/js/2.e2136cf5.chunk.js"
+  },
+  {
+    "revision": "e052f9bf24cba7f1b726",
+    "url": "/fuckts-maps/static/css/main.559b3364.chunk.css"
+  },
+  {
+    "revision": "17b5a1b38737f938f2c8",
+    "url": "/fuckts-maps/static/css/2.3cae298c.chunk.css"
+  },
+  {
+    "revision": "dc14df666a3ffb5dce52cfef30426ad8",
+    "url": "/fuckts-maps/index.html"
+  }
+];
