b9c38c620d0667787f9a55c08f06a477f7cf5dcc
Vadim Tcaregorodtcev <dev.tcaregorodtcev@gmail.com>
2019-11-18 22:32:34 +0300

    prepare preact

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Router } from 'react-router-dom';
 
 import './App.css';
@@ -6,25 +6,12 @@ import './App.css';
 import { history } from './helpers';
 import Routes from './routes';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-
-    history.listen(location => {
-      // eslint-disable-next-line no-console
-      console.log(location);
-    });
-  }
-
-  render() {
-    return (
-      <div className="app">
-        <Router history={history}>
-          <Routes />
-        </Router>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div className="app">
+    <Router history={history}>
+      <Routes />
+    </Router>
+  </div>
+);
 
 export default App;
